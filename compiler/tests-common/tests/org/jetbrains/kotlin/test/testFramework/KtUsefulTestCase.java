ff735f86843aad00631201705f2097e8237ee445
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2018-03-24 20:35:46 +0300

    tmp

--- compiler/tests-common/tests/org/jetbrains/kotlin/test/testFramework/KtUsefulTestCase.java
+++ compiler/tests-common/tests/org/jetbrains/kotlin/test/testFramework/KtUsefulTestCase.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.test.testFramework;
@@ -19,6 +8,7 @@ package org.jetbrains.kotlin.test.testFramework;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.application.impl.ApplicationInfoImpl;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Comparing;
@@ -66,6 +56,10 @@ public abstract class KtUsefulTestCase extends TestCase {
     private Application application;
 
     static {
+        String ideaHomePath = System.getProperty(PathManager.PROPERTY_HOME_PATH);
+        if (ideaHomePath == null) {
+            System.setProperty(PathManager.PROPERTY_HOME_PATH, ".");
+        }
         Logger.setFactory(TestLoggerFactory.class);
     }
 
